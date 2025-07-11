0b406428a3da030a191afdb020d8b1751bf181f9
Nur Mohammad Apu <mdapu2003@gmail.com>
2025-07-11 11:45:09 +0600

    Update .gitignore, app.js and package files

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+import dontev from "dotenv";
 import mysql from "mysql2/promise";
 
-// 1: connect to mysql server 
+dontev.config();
+
+// 1: connect to mysql server
 const db = await mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"@Apu1234",
-    database:"mysql_db",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 console.log("MySql Connected Successfully");
 
@@ -13,13 +16,56 @@ console.log("MySql Connected Successfully");
 // await db.execute(`create database mysql_db`);
 // console.log( await db.execute('show databases'));
 
-
-// 3: then we need to create a table 
+// 3: then we need to create a table
 await db.execute(`
-    create table users(
+    CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
     )
     `);
-// 4: need to perform CURD operations 
+console.log("Table ready");
+
+// 4: need to perform CURD operations
+// await db.execute(
+//     `insert into users(username, email) values(?,?)`,
+//     ['Dipu','dipu@gmail.com']
+// );
+//     console.log("Data insert successfully");
+
+// const values = [
+//   ["Apu", "apu@testmail.com"],
+//   ["Dipu", "dipu@testmail.com"],
+//   ["Rafi", "rafi@testmail.com"],
+//   ["Nitu", "nitu@testmail.com"],
+//   ["Mim", "mim@testmail.com"],
+//   ["Tushar", "tushar@testmail.com"],
+// ];
+
+// await db.query("insert into users (username,email) values(?,?)",
+//     [values]);
+
+const [rows] = await db.execute(`select * from users`);
+console.log(rows);
+
+// update
+// try {
+//     const [rows] = await db.execute(
+//         `update users set username= ? where email= ?`,
+//         ["Nur Mohammad Apu", "dipu@gmail.com"]
+//     )
+//     console.log("All users:", rows);
+// } catch (err) {
+//     console.error(err);
+// }
+
+
+// delete
+// try {
+//     const [rows] = await db.execute(
+//         `DELETE FROM users WHERE email = ?`,["dipu@gmail.com"]
+//     )
+//     console.log("All users:", rows);
+// } catch (err) {
+//     console.error(err);
+// }
